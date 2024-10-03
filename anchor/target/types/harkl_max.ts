fb3989df38b326ae7e35cf59842ea4eb454bb75a
harkl <3zy3@protonmail.ch>
2024-10-03 18:26:51 +1300

    run tests and gen idl

--- anchor/target/types/harkl_max.ts
+++ anchor/target/types/harkl_max.ts
@@ -5,19 +5,28 @@
  * IDL can be found at `target/idl/harkl_max.json`.
  */
 export type HarklMax = {
-  address: '5m94oNVhSyFesxHorRsDCAv7VYYxAcftmDc1sAQa8JPh';
-  metadata: {
-    name: 'harklMax';
-    version: '0.1.0';
-    spec: '0.1.0';
-    description: 'Created with Anchor';
-  };
-  instructions: [
+  "address": "5m94oNVhSyFesxHorRsDCAv7VYYxAcftmDc1sAQa8JPh",
+  "metadata": {
+    "name": "harklMax",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'greet';
-      discriminator: [203, 194, 3, 150, 228, 58, 181, 62];
-      accounts: [];
-      args: [];
+      "name": "greet",
+      "discriminator": [
+        203,
+        194,
+        3,
+        150,
+        228,
+        58,
+        181,
+        62
+      ],
+      "accounts": [],
+      "args": []
     }
-  ];
+  ]
 };
